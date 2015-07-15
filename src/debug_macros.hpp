d2dfbd351702d2576866c66476879c0ac9eacd2d
Koncord <koncord@rwa.su>
2015-07-16 02:37:42 +0900

    Show dev messages only for developers (_DEVMESSAGES)

--- src/debug_macros.hpp
+++ src/debug_macros.hpp
@@ -12,12 +12,15 @@
 //------------------------------------------------------------------------------
 // FIXMEs / TODOs / NOTE macros
 //------------------------------------------------------------------------------
+#ifdef _DEVMESSAGES
 #define DO_PRAGMA(x) _Pragma (#x)
 #define _QUOTE(x) # x
 #define QUOTE(x) _QUOTE(x)
 #define __FILE__LINE__ __FILE__ "(" QUOTE(__LINE__) ") : "
 #define __Message(x) DO_PRAGMA(message(__FILE__LINE__ x))
-
+#else
+    #define __Message(x)
+#endif
 #define NOTE( x ) __Message( #x )
 
 #define TODO( x ) __Message( "\n" \
@@ -30,7 +33,6 @@
 	" -------------------------------------------------\n")
 #define todo( x ) __Message( " TODO : " #x "\n" ) 
 #define fixme( x ) __Message( " FIXME: " #x "\n" )
-
 
 #endif	/* DEBUG_MACROS_HPP */
 
